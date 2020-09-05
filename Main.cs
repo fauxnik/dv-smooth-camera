a0c7335b94c81714e03d29b399fc0c7012a426a0
Niko Fox <niko@fauxnik.me>
2020-09-05 16:20:18 -0700

    integrate with ProSky mod

--- Main.cs
+++ Main.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using AwesomeTechnologies.VegetationSystem;
+using UnityEngine;
 using UnityModManagerNet;
 
 namespace SmoothCamera
@@ -8,6 +9,10 @@ namespace SmoothCamera
     {
         public static Settings settings;
 
+        private static bool isInitialized = false;
+        private static bool isActive = false;
+        private static bool isActivationRequested = false;
+
         static bool OnLoad(UnityModManager.ModEntry modEntry)
         {
             try { settings = Settings.Load<Settings>(modEntry); } catch { }
@@ -16,25 +21,57 @@ namespace SmoothCamera
             modEntry.OnSaveGUI = OnSaveGUI;
             modEntry.OnUnload = OnUnload;
             modEntry.OnToggle = OnToggle;
+            modEntry.OnUpdate = OnUpdate;
 
-            if (modEntry.Active) { SmoothTracking.SetupSmoothedCamera(); }
+            isActivationRequested = modEntry.Active;
 
             return true;
         }
 
         static bool OnUnload(UnityModManager.ModEntry modEntry)
         {
-            if (modEntry.Active) { SmoothTracking.TeardownSmoothedCamera(); }
+            if (isActive) { SmoothTracking.TeardownSmoothedCamera(); }
 
             return true;
         }
 
         static bool OnToggle(UnityModManager.ModEntry modEntry, bool isTogglingOn)
         {
-            if (isTogglingOn) { SmoothTracking.SetupSmoothedCamera(); }
-            else { SmoothTracking.TeardownSmoothedCamera(); }
+            isActivationRequested = isTogglingOn;
 
             return true;
+        }
+
+        static void OnUpdate(UnityModManager.ModEntry modEntry, float deltaMs)
+        {
+            if (!isInitialized)
+            {
+                var mainCam = Camera.main;
+                var veggieSys = UnityEngine.Object.FindObjectOfType<VegetationSystemPro>();
+                if (mainCam == null || veggieSys == null) { return; }
+
+                var proSkyModEntry = UnityModManager.FindMod("ProceduralSkyMod");
+                var isProSkyModActive = proSkyModEntry != null && proSkyModEntry.Loaded && proSkyModEntry.Active;
+                if (isProSkyModActive)
+                {
+                    var skyCam = GameObject.Find("SkyCam");
+                    var clearCam = GameObject.Find("ClearCam");
+                    if (skyCam == null || clearCam == null) { return; }
+                }
+
+                isInitialized = true;
+            }
+
+            if (isActivationRequested && !isActive)
+            {
+                SmoothTracking.SetupSmoothedCamera();
+                isActive = true;
+            }
+            else if (isActive && !isActivationRequested)
+            {
+                SmoothTracking.TeardownSmoothedCamera();
+                isActive = false;
+            }
         }
 
         static void OnGUI(UnityModManager.ModEntry modEntry) { settings.Draw(modEntry); }
--- SmoothTracking.cs
+++ SmoothTracking.cs
@@ -1,7 +1,9 @@
-﻿using System;
+﻿using AwesomeTechnologies.VegetationSystem;
+using System;
+using System.Threading.Tasks;
 using UnityEngine;
 using UnityEngine.XR;
-using AwesomeTechnologies.VegetationSystem;
+using UnityModManagerNet;
 
 namespace SmoothCamera
 {
@@ -9,47 +11,33 @@ namespace SmoothCamera
     {
         public static void SetupSmoothedCamera()
         {
-            Main.Log(">>> >>> >>> Setting up camera...");
+            if (!VRManager.IsVREnabled())
+            {
+                Main.LogWarning("[SmoothCamera] >>> Game running in non-VR mode. Skipping camera setup.");
+                return;
+            }
 
             if (GameObject.Find("SmoothCamera") != null)
             {
-                Main.LogWarning("SmoothCamera already exists. Skipping camera setup.");
+                Main.LogWarning("[SmoothCamera] >>> SmoothCamera already exists. Skipping camera setup.");
                 return;
             }
 
-            var mainCam = Camera.main;
-            VegetationSystemPro veggieSys = UnityEngine.Object.FindObjectOfType<VegetationSystemPro>();
-            if (mainCam == null || veggieSys == null || !VRManager.IsVREnabled())
-            {
-                WorldStreamingInit.LoadingFinished += DelayedSetup;
-                return;
-            }
+            var proSkyModEntry = UnityModManager.FindMod("ProceduralSkyMod");
+            var isProSkyModLoaded = proSkyModEntry != null && proSkyModEntry.Loaded && proSkyModEntry.Active;
 
-            var smoothCam = new GameObject { name = "SmoothCamera" }.AddComponent<Camera>();
-            smoothCam.CopyFrom(mainCam);
-            smoothCam.stereoTargetEye = StereoTargetEyeMask.None;
-            smoothCam.depth = mainCam.depth + 1;
-            smoothCam.gameObject.AddComponent<SmoothTracking>().trackedCam = mainCam;
-
-            veggieSys.AddCamera(smoothCam);
-        }
-
-        static void DelayedSetup()
-        {
-            Main.Log(">>> >>> >>> Delayed setup...");
-
-            WorldStreamingInit.LoadingFinished -= DelayedSetup;
-            SetupSmoothedCamera();
+            MainCamSetup();
+            if (isProSkyModLoaded) { ProSkyCamSetup(); }
         }
 
         public static void TeardownSmoothedCamera()
         {
-            Main.Log(">>> >>> >>> Tearing down camera...");
+            Main.Log("[SmoothCamera] >>> Tearing down camera...");
 
             var smoothCam = GameObject.Find("SmoothCamera");
             if (smoothCam == null)
             {
-                Main.LogWarning("SmoothCamera not found. Skipping camera teardown.");
+                Main.LogWarning("[SmoothCamera] >>> SmoothCamera not found. Skipping camera teardown.");
                 return;
             }
 
@@ -57,6 +45,48 @@ namespace SmoothCamera
             vegetationSystemPro.RemoveCamera(smoothCam.GetComponent<Camera>());
 
             Destroy(smoothCam);
+
+            var smoothSkyCam = GameObject.Find("SmoothSkyCamera");
+            if (smoothSkyCam != null) { Destroy(smoothSkyCam); }
+            var smoothClearCam = GameObject.Find("SmoothClearCamera");
+            if (smoothClearCam != null) { Destroy(smoothClearCam); }
+        }
+
+        static void MainCamSetup()
+        {
+            Main.Log("[SmoothCamera] >>> Setting up main camera...");
+
+            var veggieSys = UnityEngine.Object.FindObjectOfType<VegetationSystemPro>();
+            var smoothCam = SetupCamera("SmoothCamera", Camera.main);
+            veggieSys.AddCamera(smoothCam);
+        }
+
+        static void ProSkyCamSetup()
+        {
+            Main.Log("[SmoothCamera] >>> Setting up ProSky cameras...");
+
+            var skyCam = GameObject.Find("SkyCam");
+            var clearCam = GameObject.Find("ClearCam");
+
+            if (skyCam == null || clearCam == null)
+            {
+                Main.LogError("[SmoothCamera] >>> Could not find cameras from Procedural Sky mod!");
+                return;
+            }
+
+            SetupCamera("SmoothSkyCamera", skyCam.GetComponent<Camera>());
+            SetupCamera("SmoothClearCamera", clearCam.GetComponent<Camera>());
+        }
+
+        static Camera SetupCamera(string name, Camera target)
+        {
+            if (target == null) { return null; }
+            var smoothCam = new GameObject { name = name }.AddComponent<Camera>();
+            smoothCam.CopyFrom(target);
+            smoothCam.stereoTargetEye = StereoTargetEyeMask.None;
+            smoothCam.depth = target.depth + 20;
+            smoothCam.gameObject.AddComponent<SmoothTracking>().trackedCam = target;
+            return smoothCam;
         }
 
         void Initialize()
