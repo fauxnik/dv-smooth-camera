c039c7e484d57d294e8afbf1311997421f2f9fcc
Niko Fox <niko@fauxnik.me>
2020-09-07 19:10:14 -0700

    change default settings

--- Settings.cs
+++ Settings.cs
@@ -12,11 +12,11 @@ namespace SmoothCamera
         false;
 #endif
         [Draw(Label = "Field Of View", Min = 30, Max = 360)]
-        public float fieldOfView = 75;
+        public float fieldOfView = 90;
         [Draw(Label = "Smoothing Time - Position (values other than 0 may have undesirable results)", Min = 0)]
         public float smoothTimePosition = 0f;
         [Draw(Label = "Smoothing Time - Rotation", Min = 0)]
-        public float smoothTimeRotation = 0.5f;
+        public float smoothTimeRotation = 0.7f;
 
         override public void Save(UnityModManager.ModEntry entry)
         {
